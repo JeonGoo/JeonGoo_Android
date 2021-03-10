c992723f5bfa5c1ff1fcb2b96bd99da865bb775e
garam918 <62510764+garam918@users.noreply.github.com>
2021-03-10 15:56:12 +0900

    Feat : 로그인 화면 구성

---
+++ app/src/main/java/com/example/garam/jeongoo/network/NetworkService.kt
@@ -0,0 +1,4 @@
+package com.example.garam.jeongoo.network
+
+interface NetworkService {
+}
---
+++ app/src/main/java/com/example/garam/jeongoo/viewModel/JeonGooViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.garam.jeongoo.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class JeonGooViewModel(application: Application) : AndroidViewModel(application) {
+}
