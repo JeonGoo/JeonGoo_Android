68933d1ea8c42d741381665e35ec0902929b0cc6
garam918 <62510764+garam918@users.noreply.github.com>
2021-03-17 14:45:09 +0900

    Feat : 제품 사진을 촬영 하는 Fragment를 생성할 때 animation 추가

--- app/src/main/java/com/example/garam/jeongoo/home/itemEnrollMent/ItemEnrollActivity.kt
+++ app/src/main/java/com/example/garam/jeongoo/home/itemEnrollMent/ItemEnrollActivity.kt
@@ -9,6 +9,7 @@ class ItemEnrollActivity : AppCompatActivity() {
 
     fun replaceFragment(fragment: Fragment,parentFragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(R.anim.slide_in,R.anim.fade_out,R.anim.fade_in,R.anim.slide_out)
             add(R.id.enrollFrameLayout,fragment)
             hide(parentFragment)
             commit()
