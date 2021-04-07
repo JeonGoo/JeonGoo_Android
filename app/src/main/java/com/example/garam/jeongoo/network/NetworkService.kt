cd2e3b1c42c38f05eaee492c0c2bda0dab5d587a
garam918 <62510764+garam918@users.noreply.github.com>
2021-04-07 15:40:57 +0900

    Feat : 로그인과 회원가입과 관련된 통신 Meth0od 정의

--- app/src/main/java/com/example/garam/jeongoo/network/NetworkService.kt
+++ app/src/main/java/com/example/garam/jeongoo/network/NetworkService.kt
@@ -1,6 +1,21 @@
 package com.example.garam.jeongoo.network
 
+import com.example.garam.jeongoo.data.SignInData
+import com.example.garam.jeongoo.data.SignUpInfoData
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.*
+
 interface NetworkService {
 
+    @POST("/v1/signup")
+    fun signUp(
+        @Body signUpInfo : SignUpInfoData
+    ) : Call<ResponseBody>
+
+    @POST("/v1/signin")
+    fun signIn(
+        @Body signInInfo : SignInData
+    ) : Call<ResponseBody>
 
 }
