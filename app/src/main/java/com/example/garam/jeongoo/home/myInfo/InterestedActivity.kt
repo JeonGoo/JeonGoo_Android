9e0e69adbe302cddbdf6bab13a12ad50f441689f
garam918 <62510764+garam918@users.noreply.github.com>
2021-05-20 16:39:16 +0900

    Feat : 관심 상품 조회를 위한 Activity 생성

---
+++ app/src/main/java/com/example/garam/jeongoo/home/myInfo/InterestedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.garam.jeongoo.home.myInfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.garam.jeongoo.R
+
+class InterestedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interested)
+    }
+}
